9cc268b1a87cfc2873155b78331c7f561bcfa618
Briandemon <40809452+Briandemon@users.noreply.github.com>
2019-02-08 21:33:47 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,6 +3,15 @@ Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
-{
-	
+{    
+	var importe;
+    var descuento;
+   
+   
+    importe = document.getElementById("importe").value;
+    importe = parseInt(importe);
+    descuento = (importe *0.25);
+    resultado = (importe - descuento);
+    document.getElementById("resultado").value= resultado;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,16 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
+    var numUno;
+    var numDos;
+    var resto;
+
+    numUno = parseInt(document.getElementById("numeroDividendo").value);
+    numDos = parseInt(document.getElementById("numeroDivisor").value);
+
+    resto = (numUno%numDos);
+
+    alert("el resto es " + resto);
+
 	
 }
