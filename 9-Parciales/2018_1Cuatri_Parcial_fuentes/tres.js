970fba71b90801b417260d7a395d021e82bc40b4
Briandemon <40809452+Briandemon@users.noreply.github.com>
2019-02-08 22:15:13 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,21 @@
+//Bienvenidos. 
+//Pedir por prompt el precio y el porcentaje de descuento, mostrar el precio final con descuento por id.
+
+
+
+
 function mostrar()
 {
+
+var precio;
+var descuento;
+var preFinal;
+
+precio = prompt(precio)
+descuento = prompt(descuento);
+preFinal= (precio /100 * descuento);
+
+document.getElementById("elPrecioFinal").value=preFinal;
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,19 @@
 
+//Bienvenidos. 
+//Realizar el algoritmo que pida el ancho y el largo de un rectángulo 
+//por prompt y que muestre el perímetro por alert.
+
+
+
+
 function mostrar()
 {
 
+    var ancho;
+    var largo;
+    var perimetro;
+largo = prompt(largo);
+ancho = prompt(ancho);
+perimetro = largo*ancho;
+alert("el perimetro es "+ perimetro);
 }
