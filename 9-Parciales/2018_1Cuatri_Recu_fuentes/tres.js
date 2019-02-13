b1c1129f1dd86dbea5f9742d33ef8ed0a5e3e5b9
Briandemon <40809452+Briandemon@users.noreply.github.com>
2019-02-13 19:27:42 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,26 @@
+
+//Bienvenidos. 
+//Pedir por prompt el precio y el porcentaje de descuento, mostrar el precio final con descuento por id.
+
+
+
+
 function mostrar()
 {
+var precio;
+var porcentaje;
+var descuento;
+
+
+precio = prompt("ingrese un precio");
+
+porcentaje = prompt("Ingrese un porcentaje");
+
+descuento = (precio /100 * porcentaje  );
+precFinal = precio - descuento;
+
+document.getElementById("elPrecioFinal").value = precFinal ;
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
+var ancho;
+var largo;
+var perimetro;
+
+ancho = prompt("Ingrese un ancho");
+largo = prompt("Ingrese un largo");
+
+perimetro = (ancho*largo);
+
+alert ("el perimeto de rectangulo es "+perimetro);
+
 
 }
