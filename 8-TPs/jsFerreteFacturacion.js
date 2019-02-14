7ab5f3cf4a2969f6b38a9a6cc628c614a2d4a71d
Briandemon <40809452+Briandemon@users.noreply.github.com>
2019-02-13 22:06:38 -0300

    Add files via upload

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,66 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+    var preUno;
+    var preDos;
+    var preTres;
+    var resultado;
+
+    preUno = parseInt(document.getElementById("PrecioUno").value);
+    
+    preDos = parseInt(document.getElementById("PrecioDos").value);
+
+    preTres = parseInt(document.getElementById("PrecioTres").value);
+
+
+
+    resultado = (preUno + preDos + preTres);
+
+    alert ("La suma de los productos es " + resultado);
 	
 }
 function Promedio () 
 {
+    var preUno;
+    var preDos;
+    var preTres;
+    var resultado;
+     var promedio;
+
+    preUno = parseInt(document.getElementById("PrecioUno").value);
+
+    preDos = parseInt(document.getElementById("PrecioDos").value);
+
+    preTres = parseInt(document.getElementById("PrecioTres").value);
+    
+    resultado = preUno+ preDos+preTres;
+
+    
+
+   
 	
 }
 function PrecioFinal () 
 {
-	
+    var preUno;
+    var preDos;
+    var preTres;
+    var resultado;
+    
+    preUno = parseInt(document.getElementById("PrecioUno").value);
+
+    preDos = parseInt(document.getElementById("PrecioDos").value);
+
+    preTres = parseInt(document.getElementById("PrecioTres").value);
+
+    resultado = preUno+preDos+preTres;
+    
+       iva = resultado * 0.21;
+
+      precFinal =resultado + iva;
+
+     alert ("precio mas el iva es " +precFinal);
+
+
+
 }
