4351268006c890913fb2a7bad15f57a1c58eb97d
Briandemon <40809452+Briandemon@users.noreply.github.com>
2019-02-13 22:05:25 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -3,4 +3,25 @@ function mostrar()
 //tomo la edad  
 
 
+var edad;
+
+
+edad = document.getElementById("edad").value;
+
+edad = parseInt(edad);
+
+
+if (edad >=18)
+{
+
+
+    alert("Es mayor de edad ");
+} else {
+
+    alert("es menor de edad ");
+}
+
+
+
+
 }//FIN DE LA FUNCIÓN
