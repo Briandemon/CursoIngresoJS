c4ff4d253ecd800318090c7e5d3b0dfce55f15b1
Briandemon <40809452+Briandemon@users.noreply.github.com>
2019-03-01 21:27:31 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,55 @@
 function mostrar()
 {
 
+var veloKilomentros;
+var tipoCombustible;
+var promVelocidades;
+var veloMasBaja;
+var tipoCombustible;
+var veloCombustibleLiquido;
+var combustible;
+var contador=0;
+var tipoCombustibleMin;
+var veloMaxima;
+velovidad= parseInt(prompt("Ingrese una velocidad"));
+if(velovidad >=0 && velovicidad <= 250 ||isNaN(velocidad) )
+
+
+while (contador <= 5){
+		contador= parseInt(prompt("Ingrese un vehiculos"));
+
+		acumulador = acumulador + vehiculos;
+
+		contador = contador++;
+	}
+promedio = acumulador / 5;
+
+combustible = prompt("ingrese S ó l");
+
+
+ while (!(combustible == "S" || combustible == "L")){
+     
+    combustible = prompt("Letra incorrecta. Ingrese una letra entre S ò l.")
+
+}
+
+if (contador ==1){
+
+
+    veloMasBaja= velocidad;
+    tipoCombustibleMin = combustible;
+}else {
+
+    if(velovicidad < veloMin){
+
+        veloMin=velocidad;
+        tipoCombustibleMin = combustible;
+    }
+}if(combustible =="l" && velocidad >100){
+
+
+    cantCombustible++;
+}if(velocidad> velmaxima && combustible == "L"){
+    veloMaxima = velocidad;
+}
 }
